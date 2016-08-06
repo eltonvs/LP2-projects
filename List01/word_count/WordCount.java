ec0a4f575068cfafe87d94f452a37dd2627fdcf0
Elton Viana <eltonvs@outlook.com>
2016-08-05 21:04:33 -0300

    Code improvements

--- List01/word_count/WordCount.java
+++ List01/word_count/WordCount.java
@@ -1,20 +1,14 @@
-import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class WordCount {
     private Map<String, Integer> words;
 
     public WordCount(String text) {
-        this.words = new HashMap<String, Integer>();
+        this.words = new LinkedHashMap<String, Integer>();
 
         for (String word: text.split(" ")) {
-            if (this.words.containsKey(word)) {
-                // Increment
-                this.words.put(word, this.words.get(word) + 1);
-            } else {
-                // Add
-                this.words.put(word, 1);
-            }
+            this.words.put(word, (this.words.containsKey(word) ? this.words.get(word) + 1 : 1));
         }
     }
 
