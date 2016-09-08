db76d033c9f0ac019a83b4a651798c6d3d577f3d
Elton Viana <eltonvs@outlook.com>
2016-09-08 19:27:58 -0300

    Added today's exercise

---
+++ InterfaceAbstractClass/Exercicio01/src/Quadrado.java
@@ -0,0 +1,21 @@
+public class Quadrado implements AreaCalculavel {
+	private double side;
+
+	public Quadrado(double side) {
+		super();
+		this.side = side;
+	}
+
+	public double getSide() {
+		return side;
+	}
+
+	public void setSide(double side) {
+		this.side = side;
+	}
+
+	@Override
+	public double calcularArea() {
+		return side * side;
+	}
+}
---
+++ InterfaceAbstractClass/Exercicio01/src/Retangulo.java
@@ -0,0 +1,31 @@
+public class Retangulo implements AreaCalculavel {
+	private double width;
+	private double height;
+
+	public Retangulo(double width, double height) {
+		super();
+		this.width = width;
+		this.height = height;
+	}
+
+	public double getWidth() {
+		return width;
+	}
+
+	public void setWidth(double width) {
+		this.width = width;
+	}
+
+	public double getHeight() {
+		return height;
+	}
+
+	public void setHeight(double height) {
+		this.height = height;
+	}
+
+	@Override
+	public double calcularArea() {
+		return width*height;
+	}
+}
