69cb8362b9f0aaa83d941bfb02df2a70859054ac
Elton Viana <eltonvs@outlook.com>
2016-09-21 00:06:23 -0300

    Added new features

--- AgendaContatos/src/Aplicacao.java
+++ AgendaContatos/src/Aplicacao.java
@@ -9,5 +9,34 @@ public class Aplicacao {
 		c1.getEndereco().setNumero("100");
 		c1.getEndereco().setCidade("Natal");
 		c1.getEndereco().setEstado("RN");
+
+		Agenda a = new Agenda();
+		a.setContato1(c1);
+
+		imprimirNomes(a);
+		imprimirCnpj(a);
+	}
+
+	private static void imprimirNomes(Agenda a) {
+		if (a.getContato1() != null)
+			System.out.println(a.getContato1().getNome());
+		if (a.getContato2() != null)
+			System.out.println(a.getContato2().getNome());
+		if (a.getContato2() != null)
+			System.out.println(a.getContato3().getNome());
+	}
+
+	private static void imprimirCnpj(Agenda a) {
+		ContatoPJ c = (ContatoPJ) a.getContato1();
+		if (c != null)
+			System.out.println(c.getCnpj());
+
+		c = (ContatoPJ) a.getContato1();
+		if (c != null)
+			System.out.println(c.getCnpj());
+
+		c = (ContatoPJ) a.getContato1();
+		if (c != null)
+			System.out.println(c.getCnpj());
 	}
 }
